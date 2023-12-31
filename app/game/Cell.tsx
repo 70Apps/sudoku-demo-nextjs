8a7b5bfb0789ec2efcdbe416399a141862b9185c
老林 <cio@gpwzw.com>
2023-12-31 20:34:01 +0800

    dev(1.1):20231231

--- app/game/Cell.tsx
+++ app/game/Cell.tsx
@@ -1,69 +1,68 @@
-// 在服务器组件中添加"use client"指令
-"use client"
-import type { Metadata } from 'next'
+'use client'
+import React from 'react';
+import { useState, useEffect } from 'react';
 import classNames from 'classnames';
-import { checkSolutionValidity, TCoordinates } from '../../lib/sudoku';
+import { shallow } from 'zustand/shallow';
+import { checkSolutionValidity, TCoordinates } from '@/lib/sudoku';
 
 
 type TCellProps = {
-    puzzleValue: string | number;
+    puzzleValue: string;
     coor: TCoordinates;
     isDisabled: boolean;
-  };
+    onCellChange:(puzzleValue:string,coor:TCoordinates)=>void;
+};
 
-  const fillPuzzle = (val: string, coor: TCoordinates) => {
-    if (!val || val === '0') {
-      //removeSolution(coor);
-    }
-    // if (parseInt(val) > 0) {
-    //   // when input is valid, create coorStr
-    //   // and check solution validity
-    //   const coorStr = `${coor.x}${coor.y}`;
-    //   const isSolutionForCellValid = checkSolutionValidity(val, coor, solution);
+const Cell = ({ puzzleValue, coor, isDisabled, onCellChange}: TCellProps) => {
 
-    //   // solution is invalid, and coordinate not stored in invalidCoors
-    //   if (!isSolutionForCellValid && !invalidCoors.includes(coorStr)) {
-    //     // store `coorStr` to `invalidCoors`
-    //     addInvalidCoors(coorStr);
-    //   }
+    const [thisPuzzleValue, setThisPuzzleValue] = useState(puzzleValue);
 
-    //   // solution is valid, but `coorStr` previously deemed as invalid
-    //   if (isSolutionForCellValid && invalidCoors.includes(coorStr)) {
-    //     // remove `coorStr` from `invalidCoors`
-    //     // update `invalidCoors` value
-    //     removeInvalidCoors(coorStr);
-    //   }
-
-    //   fillSolution(val, coor);
-    // }
-  };
-
-  const handleInput =  (e: React.FormEvent<HTMLInputElement>) => {
-    const newValue = e.currentTarget.value;
-    console.log(newValue);
-    //self.puzzleValue = newValue;
+    if (!(thisPuzzleValue == puzzleValue)) {
+        setThisPuzzleValue(puzzleValue);
     }
 
-  const Cell = ({ puzzleValue, coor, isDisabled }: TCellProps) => {
+    const handleInput = (inputValue: string, inputCoor: TCoordinates) => {
+        if (!!parseInt(inputValue)) {
+            console.log('handleInput +');
+            console.log(inputValue);
+            setThisPuzzleValue(inputValue);
+            onCellChange(inputValue,{
+                x: coor.x,
+                y: coor.y,
+            });
+        }
+        else {
+            setThisPuzzleValue('');
+        }
+    }
+
     return (
-      <div
-      id={`sudoku-cell-${coor.x}${coor.y}`}
-      className={classNames('flex aspect-square items-center border text-red-700', {
-        'border-b-gray-700': (coor.x + 1) % 3 === 0 && coor.x < 8,
-        'border-r-gray-700': (coor.y + 1) % 3 === 0 && coor.y < 8,
-        //'text-red-700': invalidCoors.includes(`${coor.x}${coor.y}`), // mark text number to red when invalid
-      })}>
-        <input
-          className="h-full w-full grow text-center text-lg font-bold sm:text-xl md:text-2xl"
-          type="number"
-          min={0}
-          max={9}
-          maxLength={1} 
-          value={puzzleValue}
-          onChange={handleInput}
-          />
-      </div>
+        <div
+            id={`sudoku-cell-${coor.x}${coor.y}`}
+            className={classNames('flex aspect-square items-center border text-blue-700', {
+                'border-b-gray-700': (coor.x + 1) % 3 === 0 && coor.x < 8,
+                'border-r-gray-700': (coor.y + 1) % 3 === 0 && coor.y < 8,
+                //'text-red-700': invalidCoors.includes(`${coor.x}${coor.y}`), // mark text number to red when invalid
+            })}>
+            <input
+                className="h-full w-full grow text-center text-2xl font-bold sm:text-2xl md:text-2xl"
+                type="number"
+                min={0}
+                max={9}
+                maxLength={1}
+                value={thisPuzzleValue}
+                onChange={(e) =>
+                    void handleInput(
+                        e.target.value.split('').pop() as string, // making sure only single digit is being inputted
+                        {
+                            x: coor.x,
+                            y: coor.y,
+                        }
+                    )
+                }
+            />
+        </div>
     )
-  }
-  
+}
+
 export default Cell;
